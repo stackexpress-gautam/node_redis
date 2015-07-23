c78b01dbb30da0a36afe85d853b263f9f95ce34c
Benjamin Coe <ben@npmjs.com>
2015-07-22 22:07:38 -0700

    added a few more commands, to demonstrate my idea

--- test/mocha/commands/generated-commands.spec.js
+++ test/mocha/commands/generated-commands.spec.js
@@ -10,16 +10,26 @@ var commands = require("../../../lib/commands");
 // sampling of redis commands.
 var expectations = {
   append: {
+    before: function (client, done) { client.set('banana', 'phone', done); },
     arguments: ['banana', 'phone'],
-    assert: function (response) {
-      assert.strictEqual(5, response)
-    }
+    assert: function (response) { assert.strictEqual(10, response); }
   },
   auth: {
     arguments: ['fakepass'],
-    assertError: function (err) {
-      assert.notEqual(err.message.indexOf('no password is set'), -1)
-    }
+    assertError: function (err) { assert.notEqual(err.message.indexOf('no password is set'), -1); }
+  },
+  bitcount: {
+    before: function (client, done) { client.set('banana', 'phone', done); },
+    arguments: ['banana'],
+    assert: function (response) { assert.strictEqual(21, response); }
+  },
+  bitop: {
+    before: function (client, done) { client.set('banana', 'phone', done); },
+    arguments: ['AND', 'dest', 'banana'],
+    assert: function (response) { assert.strictEqual(5, response); }
+  },
+  'client list': {
+    assert: function (response) { assert.notEqual(response.indexOf('cmd=client'), -1); }
   }
 }
 
@@ -63,13 +73,28 @@ describe("The auto-generated methods", function () {
 
                 describe("the " + method + " method", function () {
                     it("calls sendCommand with whatever arguments it receives", function (done) {
-                      commandArgs.push(function (err, response) {
-                          if (expected.assert) expected.assert(response)
-                          if (expected.assertError) expected.assertError(err)
-                          return done();
-                      })
+                      if (expected.before) {
+                        expected.before(client, function () {
+                          applyAssertions();
+                        })
+                      } else {
+                        applyAssertions();
+                      }
 
-                      client[method].apply(client, commandArgs);
+                      // apply the assertions describe in the expections object:
+                      // * provide arguments in the arguments varible.
+                      // * provide a before callback to setup state.
+                      // * provide an assert callback to check the response.
+                      // * provide an assertError callback to check an error.
+                      function applyAssertions() {
+                        commandArgs.push(function (err, response) {
+                            if (expected.assert) expected.assert(response)
+                            if (expected.assertError) expected.assertError(err)
+                            return done();
+                        })
+
+                        client[method].apply(client, commandArgs);
+                      }
                     });
                 });
             });
